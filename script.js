2e960cc85d2f3932f94657f067a55bd55cefa9ed
theduckinduck <alecjmartinez12@gmail.com>
2026-01-07 20:14:21 +0000

    erik

--- script.js
+++ script.js
@@ -11,26 +11,56 @@ function encrypt() {
 
     // get key n parseInt
     let box2 = document.getElementById("key");
-    let key = parseInt(box2.innerText);
+    let key = parseInt(box2.value);
     // make empty string to store results
-    let string = "";
+    let result = "";
     // convert message to numbers w a for loop
     for (let i = 0; i < userText.length; i++) {
-        // get current char .charAt(i)
-        let currentChar = userText.charAt(i);
-        // convert to lower case .toLowerCase
-
+        // get current char .charAt(i) and turn lower case .toLowerCase()
+        let currentChar = userText.charAt(i).toLowerCase();
         // get index of char indexOf
-
+        let index = alpha.indexOf(currentChar);
         // apply formula (x + key) % 26 to get encrpyted number
-
+        let formulaResult = (index + key) % 26
         // use encrypted number as index in the array alpha[encryptedIndex]
-
+        let encryptedChar = alpha[formulaResult];
         // concat to result string
+        result += encryptedChar;
     }
     // put result in box
+    console.log(result);
+    let theBox = document.getElementById("result");
+    theBox.value = result;
 }
 
 function decrypt() {
     console.log("decrypt")
+        console.log("encrypt")
+    // get user message text
+    let box = document.getElementById("messageInput");
+    let userText = box.value;
+    console.log(userText);
+
+    // get key n parseInt
+    let box2 = document.getElementById("key");
+    let key = parseInt(box2.value);
+    // make empty string to store results
+    let result = "";
+    // convert message to numbers w a for loop
+    for (let i = 0; i < userText.length; i++) {
+        // get current char .charAt(i) and turn lower case .toLowerCase()
+        let currentChar = userText.charAt(i).toLowerCase();
+        // get index of char indexOf
+        let index = alpha.indexOf(currentChar);
+        // apply formula (x + key) % 26 to get encrpyted number
+        let formulaResult = (index - key) % 26
+        // use encrypted number as index in the array alpha[encryptedIndex]
+        let encryptedChar = alpha[formulaResult];
+        // concat to result string
+        result += encryptedChar;
+    }
+    // put result in box
+    console.log(result);
+    let theBox = document.getElementById("result");
+    theBox.value = result;
 }
